23fe3f5a0d74d27f5dfbdedd28ce87cc8a53b33c
He Jun <hejun@outlook.com>
2018-03-30 23:26:53 +0800

    增加BPF过滤器

--- main.go
+++ main.go
@@ -7,13 +7,14 @@ import (
 	"log"
 	"os"
 	"os/signal"
+	"strings"
 	"time"
 
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/pcap"
 )
 
-func doIt(ctx context.Context, device *string, promiscuous *bool) {
+func doIt(ctx context.Context, device string, promiscuous bool, expression string) {
 
 	var (
 		snapshotLen int32 = 1024
@@ -23,16 +24,24 @@ func doIt(ctx context.Context, device *string, promiscuous *bool) {
 	)
 
 	// Open device
-	handle, err = pcap.OpenLive(*device, snapshotLen, *promiscuous, timeout)
+	handle, err = pcap.OpenLive(device, snapshotLen, promiscuous, timeout)
 	if err != nil {
 		log.Fatal(err)
 		os.Exit(1)
 	}
 	defer handle.Close()
 
+	//Set BPFilter
+	if expression != "" {
+		if err := handle.SetBPFFilter(expression); err != nil {
+			fmt.Printf("Filter Expression (%v) Error!\n", expression)
+			os.Exit(1)
+		}
+	}
+
 	//Print device name and mode
-	fmt.Printf("Sniffer on %v...", *device)
-	if *promiscuous {
+	fmt.Printf("Sniffer on %v...", device)
+	if promiscuous {
 		fmt.Print("(promiscuous mode)")
 	}
 	fmt.Println()
@@ -42,15 +51,17 @@ func doIt(ctx context.Context, device *string, promiscuous *bool) {
 
 	//packet counter
 	var packetCount int64
-	for packet := range packetSource.Packets() {
-		// Process packet here
-		fmt.Println(packet)
-		packetCount++
+
+	for {
 		select {
+		case packet := <-packetSource.Packets():
+			// Process packet here
+			fmt.Println(packet)
+			packetCount++
 		case <-ctx.Done():
+			// Print packets counter and exit goroutine
 			fmt.Printf("\n%v packets captured.\n", packetCount)
 			os.Exit(0)
-		default:
 		}
 	}
 
@@ -59,12 +70,19 @@ func doIt(ctx context.Context, device *string, promiscuous *bool) {
 //main function
 func main() {
 
+	var expression string
+
 	device := flag.String("i", "eth0", "device name")
 	promiscuous := flag.Bool("p", false, "promiscuous mode")
+
 	flag.Parse()
+	expressionArgs := flag.Args()
+	if len(expressionArgs) != 0 {
+		expression = strings.Join(expressionArgs, " ")
+	}
 
 	ctx, cancel := context.WithCancel(context.Background())
-	go doIt(ctx, device, promiscuous)
+	go doIt(ctx, *device, *promiscuous, expression)
 
 	c := make(chan os.Signal)
 	signal.Notify(c, os.Kill, os.Interrupt)
