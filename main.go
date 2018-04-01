a7fbb28d67857b59199495eac9d300ad8a2fc1c9
He Jun <hejun@outlook.com>
2018-04-01 18:09:21 +0800

    增加快速读取模式

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ import (
 
 var wg sync.WaitGroup
 
-func sniff(ctx context.Context, device string, promiscuous bool, expression string) {
+func sniff(ctx context.Context, device string, promiscuous bool, expression string, block bool) {
 
 	wg.Add(1)
 	defer wg.Done()
@@ -27,12 +27,18 @@ func sniff(ctx context.Context, device string, promiscuous bool, expression stri
 	var (
 		snapshotLen int32 = 1024
 		err         error
-		timeout     time.Duration = 30 * time.Second
 		handle      *pcap.Handle
+		timeout     time.Duration = 30 * time.Second
 	)
 
 	// Open device
-	handle, err = pcap.OpenLive(device, snapshotLen, promiscuous, timeout)
+
+	if block {
+		handle, err = pcap.OpenLive(device, snapshotLen, promiscuous, pcap.BlockForever)
+	} else {
+		handle, err = pcap.OpenLive(device, snapshotLen, promiscuous, timeout)
+	}
+
 	if err != nil {
 		log.Fatal(err)
 		return
@@ -77,36 +83,13 @@ func sniff(ctx context.Context, device string, promiscuous bool, expression stri
 
 //main function
 func main() {
-
-	//-------------------性能测量---------------------
-	cpuFile, errCPU := os.OpenFile("cpu.pprof", os.O_CREATE|os.O_RDWR, 0644)
-	if errCPU != nil {
-		log.Fatal("cpu.pprof file create error")
-	}
-	defer cpuFile.Close()
-	pprof.StartCPUProfile(cpuFile)
-	defer pprof.StopCPUProfile()
-
-	memFile, errMEM := os.OpenFile("mem.pprof", os.O_CREATE|os.O_RDWR, 0644)
-	if errMEM != nil {
-		log.Fatal("mem.pprof file create error")
-	}
-	defer memFile.Close()
-	pprof.WriteHeapProfile(memFile)
-
-	traceFile, errTrace := os.OpenFile("trace.out", os.O_CREATE|os.O_RDWR, 0644)
-	if errTrace != nil {
-		log.Fatal("trace.out file create error")
-	}
-	defer traceFile.Close()
-	trace.Start(traceFile)
-	defer trace.Stop()
-
-	//-----------------main program--------------------
+	//------------解析命令行参数-------------------------
 	var expression string
 
 	device := flag.String("i", "eth0", "device name")
 	promiscuous := flag.Bool("p", false, "promiscuous mode")
+	pprofCheck := flag.Bool("c", false, "pprof mode")
+	blockFlag := flag.Bool("b", false, "read timeout=0")
 
 	flag.Parse()
 	expressionArgs := flag.Args()
@@ -114,9 +97,36 @@ func main() {
 		expression = strings.Join(expressionArgs, " ")
 	}
 
+	//-------------------性能测量---------------------
+	if *pprofCheck {
+		cpuFile, errCPU := os.OpenFile("cpu.pprof", os.O_CREATE|os.O_RDWR, 0644)
+		if errCPU != nil {
+			log.Fatal("cpu.pprof file create error")
+		}
+		defer cpuFile.Close()
+		pprof.StartCPUProfile(cpuFile)
+		defer pprof.StopCPUProfile()
+
+		memFile, errMEM := os.OpenFile("mem.pprof", os.O_CREATE|os.O_RDWR, 0644)
+		if errMEM != nil {
+			log.Fatal("mem.pprof file create error")
+		}
+		defer memFile.Close()
+		pprof.WriteHeapProfile(memFile)
+
+		traceFile, errTrace := os.OpenFile("trace.out", os.O_CREATE|os.O_RDWR, 0644)
+		if errTrace != nil {
+			log.Fatal("trace.out file create error")
+		}
+		defer traceFile.Close()
+		trace.Start(traceFile)
+		defer trace.Stop()
+	}
+
+	//-----------------main program--------------------
 	ctx, cancel := context.WithCancel(context.Background())
 
-	go sniff(ctx, *device, *promiscuous, expression)
+	go sniff(ctx, *device, *promiscuous, expression, *blockFlag)
 
 	c := make(chan os.Signal)
 	signal.Notify(c, os.Kill, os.Interrupt)
